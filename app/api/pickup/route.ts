09eb7f48aeaa22d950bdcc854139fc58d2b35060
TirthPatel8267 <tirthpatel8267@gmail.com>
2026-04-10 23:44:59 +0530

    fix email env

--- app/api/pickup/route.ts
+++ app/api/pickup/route.ts
@@ -1,12 +1,13 @@
 import connectDB from "@/lib/db";
 import Pickup from "@/models/Pickup";
-import {sendEmail} from "@/lib/mail";
+import { sendEmail } from "@/lib/mail";
 
 export async function POST(req: Request) {
   try {
     await connectDB();
 
     const body = await req.json();
+    console.log("Sending email to:", body.email); 
 
     const pickup = await Pickup.create({
       ...body,
@@ -16,6 +17,9 @@ export async function POST(req: Request) {
         lng: 72.5714,
       },
     });
+
+    // ✅ SEND EMAIL TO USER
+    await sendEmail(body.email, body);
 
     return Response.json({ success: true, pickup });
 
--- lib/mail.ts
+++ lib/mail.ts
@@ -1,18 +1,33 @@
 import nodemailer from "nodemailer";
 
-export async function sendEmail(to: string) {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: "tirthpatel8267@gmail.com", // 🔥 replace
-      pass: "btat wzgi nlgv olnw",   // 🔥 replace
-    },
-  });
+export async function sendEmail(to: string, pickupData: any) {
+  try {
+    console.log("ENV CHECK:", process.env.EMAIL_USER);
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true, // 🔥 IMPORTANT
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
 
-  await transporter.sendMail({
-    from: "E-Waste System",
-    to,
-    subject: "Pickup Request Submitted ♻",
-    text: "Your pickup request has been successfully submitted ✅",
-  });
+    const info = await transporter.sendMail({
+      from: `"E-Waste System" <${process.env.EMAIL_USER}>`,
+      to: to,
+      subject: "📦 Pickup Request Confirmed",
+      html: `
+        <h2>Pickup Confirmed ✅</h2>
+        <p>Name: ${pickupData.name}</p>
+        <p>Address: ${pickupData.address}</p>
+        <p>Date: ${pickupData.date}</p>
+      `,
+    });
+
+    console.log("EMAIL SENT:", info.messageId);
+
+  } catch (error) {
+    console.error("EMAIL ERROR:", error);
+  }
 }
